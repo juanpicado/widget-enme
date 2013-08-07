b38e2165d0ef58ad2a8df55c2eff7f755aa27b50
Juan Carlos Picado <juan@encuestame.org>
2013-08-07 23:58:39 +0200

    renamed build file

--- dev.sh
+++ dev.sh
@@ -1,1 +1,1 @@
-node components/r.js/dist/r.js -o baseUrl=. name=components/almond/almond include=src/main out=main-built.js wrap=true optimize=none
+node components/r.js/dist/r.js -o baseUrl=. name=components/almond/almond include=src/main out=widget.js wrap=true optimize=none
